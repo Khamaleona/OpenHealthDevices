78d23ac4d49b836fcdad7bc9d7b838eb95688774
Irene Carrasco Santano <ircarrasc@alumnos.unex.es>
2020-11-14 17:42:28 +0100

    Update tools.py

--- tools.py
+++ tools.py
@@ -10,8 +10,8 @@ else:
     platform = 0
     if sys.platform.startswith("win"):
         os.system('cmd /c "setx path \"%PATH%;"' + str(sys.argv[0]) + '"\""')
-        os.system('cmd /c "arduino-cli core install arduino:avr"')
-        os.system('cmd /c "arduino-cli config init && arduino-cli core update-index --additional-urls https://dl.espressif.com/dl/package_esp32_index.json"')
+        os.system('cmd /c "arduino-cli config init"')
+        os.system('cmd /c "arduino-cli core install arduino:avr && arduino-cli core update-index --additional-urls https://dl.espressif.com/dl/package_esp32_index.json"')
         os.system('cmd /c "arduino-cli core install esp32:esp32"')
         os.system('cmd /c "arduino-cli board listall esp32"')
         # adding necessary libraries
@@ -19,7 +19,7 @@ else:
         os.system('cmd /c "pip install Jinja2 && pip install pyserial"')
     elif sys.platform.startswith("linux") or sys.platform.startswith("cygwin"):
         os.system('export PATH=$PATH:' + str(sys.argv[0]))
-        commands = ["arduino-cli core install arduino:avr", "arduino-cli config init",
+        commands = ["arduino-cli config init", "arduino-cli core install arduino:avr",
                     "arduino-cli core update-index --additional-urls https://dl.espressif.com/dl/package_esp32_index.json",
                     "arduino-cli core install esp32:esp32", "arduino-cli board listall esp32",
                     "arduino-cli lib install \"M5StickC\"",
